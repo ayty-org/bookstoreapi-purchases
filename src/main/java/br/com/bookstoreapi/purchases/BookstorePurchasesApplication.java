fba8212e18728f403a5d1098138112d53043d12c
Jackson Gomes <jacksonanderson1515@hotmail.com>
2022-04-25 15:16:25 -0300

    initial commit

---
+++ src/main/java/br/com/bookstoreapi/purchases/BookstorePurchasesApplication.java
@@ -0,0 +1,13 @@
+package br.com.bookstoreapi.purchases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstorePurchasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstorePurchasesApplication.class, args);
+	}
+
+}
