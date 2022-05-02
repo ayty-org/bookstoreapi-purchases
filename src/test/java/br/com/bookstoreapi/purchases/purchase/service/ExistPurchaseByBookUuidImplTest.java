7376648f528807c89ed4eb4a6a2df882a5087b56
Jackson Gomes <jacksonanderson1515@hotmail.com>
2022-05-02 13:35:15 -0300

    created new service tests

---
+++ src/test/java/br/com/bookstoreapi/purchases/purchase/service/ExistPurchaseByBookUuidImplTest.java
@@ -0,0 +1,43 @@
+package br.com.bookstoreapi.purchases.purchase.service;
+
+import br.com.bookstoreapi.purchases.purchase.PurchaseRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+class ExistPurchaseByBookUuidImplTest {
+
+
+    private ExistPurchaseByBookUuidImpl existPurchaseByBookUuid;
+    @Mock
+    private  PurchaseRepository repository;
+
+
+    @BeforeEach
+    void setUp(){
+        this.existPurchaseByBookUuid = new ExistPurchaseByBookUuidImpl(repository);
+    }
+
+
+    @Test
+    void existsByBookUuidTest(){
+        when(repository.existsByBooksUuid(UUID.fromString("12d51c0a-a843-46fc-8447-5fda559ec69b")))
+                .thenReturn(true);
+
+
+        assertThat(existPurchaseByBookUuid.existsByBookUuid(UUID.fromString("12d51c0a-a843-46fc-8447-5fda559ec69b"))
+                , is(true));
+        assertThat(existPurchaseByBookUuid.existsByBookUuid(UUID.fromString("df670f4b-5d4d-4f70-ae78-f2ddc9fa1f14"))
+                , is(false));
+
+    }
+}
---
+++ src/test/java/br/com/bookstoreapi/purchases/purchase/service/ExistPurchaseByClientUuidImplTest.java
@@ -0,0 +1,42 @@
+package br.com.bookstoreapi.purchases.purchase.service;
+
+import br.com.bookstoreapi.purchases.purchase.PurchaseRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+class ExistPurchaseByClientUuidImplTest {
+
+    private ExistPurchaseByClientUuidImpl existPurchaseByClientUuid;
+    @Mock
+    private PurchaseRepository repository;
+
+
+    @BeforeEach
+    void setUp(){
+        this.existPurchaseByClientUuid = new ExistPurchaseByClientUuidImpl(repository);
+    }
+
+
+    @Test
+    void existsByBookUuidTest(){
+        when(repository.existsByClientUuid(UUID.fromString("12d51c0a-a843-46fc-8447-5fda559ec69b")))
+                .thenReturn(true);
+
+        assertThat(existPurchaseByClientUuid.existsByClientUuid(UUID.fromString("12d51c0a-a843-46fc-8447-5fda559ec69b"))
+                , is(true));
+        assertThat(existPurchaseByClientUuid.existsByClientUuid(UUID.fromString("df670f4b-5d4d-4f70-ae78-f2ddc9fa1f14"))
+                , is(false));
+
+    }
+}
